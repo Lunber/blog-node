d5c511ae35a91db919324f84917cb7bc373880e3
Lunber <891504539@qq.com>
2016-08-23 18:28:45 +0800

    first commit

---
+++ models/db.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lwb on 2016/8/22.
+ */
+var settings = require('../settings'),
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server;
+module.exports = new Db(settings.db, new Server(settings.host, settings.port),
+    {safe: true});
---
+++ settings.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lwb on 2016/8/22.
+ */
+module.exports = {
+    cookieSecret: 'myblog',
+    db: 'blog',
+    host: 'localhost',
+    port: 27017
+}; 
